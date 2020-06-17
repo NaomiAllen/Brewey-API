6e0d38e02b4d87b6e7bd1dcd1a65edd15f9b3104
Naomi <naomi@macbook-pro.attlocal.net>
2020-06-17 16:04:17 -0500

    styling and API functions

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+/*------------------------ Classes and Objects ------------------------------*/
+
+
+
+/*---------------------------------- Cache ----------------------------------*/
+const baseURL = 'https://api.brewerydb.com/v2/';
+const apikey = 'key=0d8881693054b317b3ab989c18a74c4d';
+const queryType = 'beers/';
+let nameQuery = '';
+let queryURL = baseURL + queryType + '?' + apikey 
+
+console.log(queryURL);
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+$(() =>{  
+
+const getBeer = () => {
+    $.ajax({
+        url: queryURL + nameQuery,
+    }).then((beersData) => {
+        $('.container').html(`
+        <h2> ${beersData.name} </h2>
+        <h3> ${beersData.discription} </h3>
+        <h4> ${beersData.abv} <h4>
+        <h5> ${beersData.isOrganic} <h5>
+        
+        `)
+        console.log(beersData)
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+getBeer();
+
+});
